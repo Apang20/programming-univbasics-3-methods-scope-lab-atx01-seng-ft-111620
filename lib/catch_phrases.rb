99058eedd9b388a06dd4e6454e35e161819cd8c6
convivial-parser-7775 <avelonpang@gmail.com>
2020-09-22 02:50:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,14 @@
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+#status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 phrase = It's-a me, Mario!'
 def mario
   #status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = It's-a me, Mario!'
   puts phrase
+  phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+  status
 end

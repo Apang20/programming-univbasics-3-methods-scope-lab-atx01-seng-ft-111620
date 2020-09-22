2743fcadcec9ae4179abf5d3a81e8d8da858441f
convivial-parser-7775 <avelonpang@gmail.com>
2020-09-22 03:00:50 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -21,6 +21,9 @@ end
 
 def all_phrases
   say = "puts out all of the previous catch phrases" 
+  puts phrase
+  puts status
+  puts quote
 It's-a me, Mario!\n/).to_stdout
     expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
     expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
